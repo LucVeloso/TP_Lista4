72319110a92fbb207258c89d69479d320c63b109
Lucas Veloso <54967198+LucVeloso@users.noreply.github.com>
2019-10-31 11:25:37 -0300

    Add files via upload

---
+++ Questão 3/L4Q3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+void cadastro(map<string, string> &t, string n){
+
+    string numero;
+
+    cout << "Informe o numero de " << n <<": ";
+    cin >> numero;
+
+    t[n] = numero;
+}
+
+bool procurarNome(map<string, string> t, string n){
+
+    if(t.find(n) != t.end()) return true;
+    else return false;
+}
+
+int main(){
+    
+    map<string, string> telefones;
+    string nome, telefone;
+    bool proc;
+
+    while(nome != "sair"){
+
+        cout << "Informe o nome: ";
+        cin >> nome;
+
+        if(nome == "sair") break;
+
+        proc = procurarNome(telefones, nome);
+
+        if(!proc) cadastro(telefones, nome);
+        else cout << nome << ": " << telefones[nome] << endl;
+    }
+
+    return 0;
+}
